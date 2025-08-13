e6ea5a8350e9e61d056a4d8ab1ba311e3e8add7c
zeroawakening <pyrodemononorion@gmail.com>
2025-08-12 23:07:29 -0400

    updated treesitter autoindent to ignore cpp. updated some plugins.

--- init.lua
+++ init.lua
@@ -907,7 +907,7 @@ require('lazy').setup({
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', 'cpp' } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
